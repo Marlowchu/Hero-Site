d94e220dbb34e5e304d3ade8d3d592effb7c0918
Chuck Marlow <marlowchu@gmail.com>
2021-12-11 15:09:34 -0500

    linking pages

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+function nextPage() {
+    // window.location.assign("./countdown.html");
+    // window.document.location = ("countdown.html")
+    window.location.href = "countdown.html";
+    console.log ("start pressed") 
+
+
+}
