9e04c17a67e692d3b639f525ecb3bd8743c0c0bd
Terrell Hudson <thudson5731@gmail.com>
2021-12-11 12:51:36 -0500

    refactoring API calls code

--- movie.js
+++ movie.js
@@ -1,12 +1,13 @@
 var head = document.querySelector('h1');
-const matrix = 'jumanji';
-const omdbUrl = `http://www.omdbapi.com/?t=${matrix}&apikey=34da3d2a`;
+const key = '34da3d2a';
+const matrix = 'the+avengers';
+const omdbUrl = `http://www.omdbapi.com/?t=${matrix}&apikey=${key}`;
 
 fetch(omdbUrl)
 	.then((response) => response.json())
 	.then((data) => {
 		const movie = data.Poster;
-		console.log(movie);
+		console.log(data.BoxOffice);
 		// var display = document.createElement('li');
 		// display.append(document.createTextNode(movie));
 		head.innerHTML = `<img src=${movie} />;`;
